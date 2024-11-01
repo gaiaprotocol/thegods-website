7fe3770d876c1aa04615bf2d0c17ecdde7ce9e8f
yjgaia <yjdev88@gmail.com>
2024-11-01 22:10:12 +0900

    Add data loading functionality to NFTViewer component

--- app/views/NFTViewer.ts
+++ app/views/NFTViewer.ts
@@ -1,5 +1,6 @@
 import { DomNode, View } from "@common-module/app";
 import { LetterboxedScreen } from "@gaiaengine/2d";
+import AppConfig from "../AppConfig.js";
 
 export default class NFTViewer extends View {
   private tokenId = new URLSearchParams(location.search).get("tokenId") ?? "";
@@ -10,5 +11,15 @@ export default class NFTViewer extends View {
     this.container = new DomNode(
       document.querySelector(".nft-viewer") as HTMLElement,
     ).append(new LetterboxedScreen(1024, 1024));
+
+    this.load();
+  }
+
+  private async load() {
+    const data = await AppConfig.supabaseConnector.safeFetchSingle(
+      "the_gods_metadatas",
+      (b) => b.select("*").eq("token_id", this.tokenId),
+    );
+    console.log(data);
   }
 }
