f4397b4bf886414be0a6ce015b6334cbdc883121
yjgaia <yjdev88@gmail.com>
2024-11-08 14:54:40 +0900

    Enhance introView with SVG icon for Valhalla link; improve visual engagement for community membership description

--- pages/introView.ts
+++ pages/introView.ts
@@ -64,10 +64,17 @@ export function introView<T>(): T {
             el(
               "p",
               "Connect and engage with our development team in real-time through ",
-              el("a", "Valhalla", {
-                href: "https://valhalla.gaia.cc",
-                target: "_blank",
-              }),
+              el(
+                "a",
+                "Valhalla ",
+                html(
+                  '<svg width="0.625rem" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 9L9 1M9 1H2.5M9 1V7.22222" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+                ),
+                {
+                  href: "https://valhalla.gaia.cc",
+                  target: "_blank",
+                },
+              ),
               ", our exclusive holder-only platform.",
             ),
           ),
