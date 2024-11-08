43757157559c65d15970da12f81fc9ef10867a2d
yjgaia <yjdev88@gmail.com>
2024-11-08 13:45:41 +0900

    Remove unused file existence check in entrypoint; simplify file serving logic

--- server/entrypoint.ts
+++ server/entrypoint.ts
@@ -1,4 +1,3 @@
-import { exists } from "https://deno.land/std@0.223.0/fs/mod.ts";
 import { serveFile } from "https://deno.land/std@0.223.0/http/file_server.ts";
 import { corsHeaders } from "https://raw.githubusercontent.com/yjgaia/deno-module/main/api.ts";
 import { pages } from "./pages.ts";
@@ -11,13 +10,9 @@ Deno.serve(async (req) => {
 
   const filePath = basePath + path;
 
-  const response = path !== "/" && await exists(filePath)
-    ? await serveFile(req, filePath)
-    : await serveFile(req, basePath + "/index.html");
-
+  const response = await serveFile(req, filePath);
   for (const [key, value] of Object.entries(corsHeaders)) {
     response.headers.set(key, value);
   }
-
   return response;
 });
